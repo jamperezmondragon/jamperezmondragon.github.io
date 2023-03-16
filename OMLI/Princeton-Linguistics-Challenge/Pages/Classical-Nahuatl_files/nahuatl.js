bd0b62501d18c18a10ec16170f4ad284b156284c
jamperezmondragon <70548653+jamperezmondragon@users.noreply.github.com>
2023-03-16 11:39:11 -0600

    Add files via upload

---
+++ OMLI/Princeton-Linguistics-Challenge/Pages/Classical-Nahuatl_files/nahuatl.js
@@ -0,0 +1,80 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+// JavaScript Document
+function testform()
+{var score=0;
+if ((document.nahuatl.tikwiikani.value=="You are singing")||(document.nahuatl.tikwiikani.value=="you are singing")||(document.nahuatl.tikwiikani.value=="You are singing.")||(document.nahuatl.tikwiikani.value=="you are singing.")) 
+	{
+	score = score + 1
+	}
+
+if ((document.nahuatl.nikwiikaya.value=="I was singing")||(document.nahuatl.nikwiikaya.value=="i was singing")||(document.nahuatl.nikwiikaya.value=="I was singing.")||(document.nahuatl.nikwiikaya.value=="i was singing."))
+	{
+	score = score + 1
+	} 
+
+if ((document.nahuatl.chookanih.value=="They are crying")||(document.nahuatl.chookanih.value=="they are crying")||(document.nahuatl.chookanih.value=="They are crying.")||(document.nahuatl.chookanih.value=="they are crying."))
+	{
+	score = score + 1
+	} 
+
+if ((document.nahuatl.theysleep.value=="kochih")||(document.nahuatl.theysleep.value=="Kochih")||(document.nahuatl.theysleep.value=="kochih.")||(document.nahuatl.theysleep.value=="Kochih."))
+	{
+	score = score + 1
+	} 
+
+if ((document.nahuatl.Iwillsleep.value=="nikochis")||(document.nahuatl.Iwillsleep.value=="Nikochis")||(document.nahuatl.Iwillsleep.value=="nikochis.")||(document.nahuatl.Iwillsleep.value=="Nikochis."))
+	{
+	score = score + 1
+	} 
+
+if ((document.nahuatl.youwillcry.value=="ticho:kas")||(document.nahuatl.youwillcry.value=="Ticho:kas")||(document.nahuatl.youwillcry.value=="ticho:kas.")||(document.nahuatl.youwillcry.value=="Ticho:kas."))
+	{
+	score = score + 1
+	} 
+
+if (score==0) {document.nahuatl.evaluation.value = "Oops!"}
+if (score==1) {document.nahuatl.evaluation.value = "Good start!"}
+if (score==2) {document.nahuatl.evaluation.value = "Keep going!"}
+if (score==3) {document.nahuatl.evaluation.value = "Way to go!"}
+if (score==4) {document.nahuatl.evaluation.value = "Almost there!"}
+if (score==5) {document.nahuatl.evaluation.value = "One more!"}
+if (score==6) {document.nahuatl.evaluation.value = "Congratulations!"; 
+				document.nahuatl.scoreoutput.value = score + " out of 6!";
+				window.location="certificate.htm"}
+	else document.nahuatl.scoreoutput.value = score + " out of 6"
+}
+
+
+}
+/*
+     FILE ARCHIVED ON 00:08:33 Nov 15, 2015 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 03:00:35 Feb 16, 2023.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 860.547
+  exclusion.robots: 0.064
+  exclusion.robots.policy: 0.058
+  cdx.remote: 0.047
+  esindex: 0.006
+  LoadShardBlock: 829.769 (3)
+  PetaboxLoader3.resolve: 852.288 (4)
+  PetaboxLoader3.datanode: 77.04 (4)
+  CDXLines.iter: 15.098 (3)
+  load_resource: 156.248
+*/
---
+++ OMLI/Princeton-Linguistics-Challenge/Pages/Swahili-DRC_files/swahilidrc.js
@@ -0,0 +1,63 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+// JavaScript Document
+function testform()
+{var score=0;
+if ((document.swahilidrc.shesawthem.value=="aliwaona")||(document.swahilidrc.shesawthem.value=="Aliwaona")||(document.swahilidrc.shesawthem.value=="aliwaona.")||(document.swahilidrc.shesawthem.value=="Aliwaona.")) 
+	{
+	score = score + 1
+	}
+
+if ((document.swahilidrc.iwillseeyou.value=="nitakawuona")||(document.swahilidrc.iwillseeyou.value=="Nitakawuona")||(document.swahilidrc.iwillseeyou.value=="nitakawuona.")||(document.swahilidrc.iwillseeyou.value=="Nitakawuona."))
+	{
+	score = score + 1
+	} 
+
+if ((document.swahilidrc.shesawme.value=="aliniona")||(document.swahilidrc.shesawme.value=="Aliniona")||(document.swahilidrc.shesawme.value=="aliniona.")||(document.swahilidrc.shesawme.value=="Aliniona."))
+	{
+	score = score + 1
+	} 
+
+if (score==0) {document.swahilidrc.evaluation.value = "Oops!"}
+if (score==1) {document.swahilidrc.evaluation.value = "Keep going!"}
+if (score==2) {document.swahilidrc.evaluation.value = "Almost there!"}
+if (score==3) {document.swahilidrc.evaluation.value = "Congratulations!"; 
+				document.swahilidrc.scoreoutput.value = score + " out of 3!";
+				window.location="certificate.htm"}
+	else document.swahilidrc.scoreoutput.value = score + " out of 3"
+}
+
+
+}
+/*
+     FILE ARCHIVED ON 17:51:24 Jan 14, 2022 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 03:10:09 Feb 16, 2023.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 131.875
+  exclusion.robots: 0.077
+  exclusion.robots.policy: 0.07
+  cdx.remote: 0.063
+  esindex: 0.009
+  LoadShardBlock: 102.327 (3)
+  PetaboxLoader3.datanode: 116.422 (5)
+  CDXLines.iter: 15.0 (3)
+  load_resource: 112.865
+  PetaboxLoader3.resolve: 35.795
+  loaddict: 60.723
+*/
